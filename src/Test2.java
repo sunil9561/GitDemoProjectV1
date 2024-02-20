1571ecdf33f14e4a35ed961e659f654f5da1e631
sunil9561 <sknaidu.m@gmail.com>
2024-02-19 23:00:47 -0600

    added the different check points and committed

--- src/Test2.java
+++ src/Test2.java
@@ -7,7 +7,8 @@ public class Test2 {
 		
 		String hungry="yes";
 		
-		if (hungry == "yes") {
+		if (hungry == "yes"|| hungry=="y" || hungry =="Y")
+		{
 			System.out.println("eat something....");
 		}else{
 			System.out.println("Do work....");
